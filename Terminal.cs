198addfcdb09b443b791de707400151069ad9f60
Sakthivel Murugan Pranesh <s10266891@connect.np.edu.sg>
2025-01-27 15:03:53 +0800

    Update Terminal.cs with code

--- Terminal.cs
+++ Terminal.cs
@@ -1,1 +1,36 @@
+namespace Programming_II_Assignment;
+//=============================================================
+// Student Number: S10266891D
+// Student Name: Sakthivel Murugan Pranesh
+// Partner Name: Kamalkannan Kavin Balaji
+//=============================================================
+public class Terminal
+{
+    public string TerminalName { get; set; }
+    public Dictionary<string, Airline> Airlines { get; set; } = new();
+    public Dictionary<string, Flight> Flights { get; set; } = new();
+    public Dictionary<string, BoardingGate> BoardingGates { get; set; } = new();
+    public double GateFees { get; set; }
+
+    public bool AddAirline(Airline airline)
+    {
+        if (!Airlines.ContainsKey(airline.Code))
+        {
+            Airlines.Add(airline.Code, airline);
+            return true;
+        }
+        return false;
+    }
+
+    public bool AddBoardingGate(BoardingGate gate)
+    {
+        if (!BoardingGates.ContainsKey(gate.GateName))
+        {
+            BoardingGates.Add(gate.GateName, gate);
+            return true;
+        }
+        return false;
+    }
+}
+
 
