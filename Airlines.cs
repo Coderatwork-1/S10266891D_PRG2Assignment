8d49374eb7111852e4fdffc79d760a2cef1d1eb6
Sakthivel Murugan Pranesh <s10266891@connect.np.edu.sg>
2025-01-27 14:59:22 +0800

    Update Airlines.cs with code

--- Airlines.cs
+++ Airlines.cs
@@ -1,1 +1,22 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+//=============================================================
+// Student Number: S10266891D
+// Student Name: Sakthivel Murugan Pranesh
+// Partner Name: Kamalkannan Kavin Balaji
+//=============================================================
+namespace Programming_II_Assignment;
+    public class Airline
+{
+    public string Code { get; set; }
+    public string Name { get; set; }
 
+    public Airline(string code, string name)
+    {
+        Code = code;
+        Name = name;
+    }
+}
