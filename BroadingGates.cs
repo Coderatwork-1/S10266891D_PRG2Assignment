27cc6783a6e4310e102e51d4be14f7c6d369f8a8
Kavin-Balaji-NP-IT <kkavinbalaji200708@gmail.com>
2025-02-02 22:55:47 +0800

    Update BroadingGates.cs

--- BroadingGates.cs
+++ BroadingGates.cs
@@ -3,41 +3,47 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace Programming_II_Assignment;
-//=============================================================
+//=========================================
 // Student Number: S10266891D
 // Student Name: Sakthivel Murugan Pranesh
 // Partner Name: Kamalkannan Kavin Balaji
-//=============================================================
-    class BoardingGate
+//========================================
+
+namespace S102266891D_PRG2Assignment
+{
+    public class BoardingGate
     {
-        public string GateName { get; set; }
-        public bool SupportsCFFT { get; set; }
-        public bool SupportsDDJB { get; set; }
-        public bool SupportsLWTT { get; set; }
-        public Flight Flight { get; set; }
+    
+            public string GateName { get; set; }
+            public bool SupportsCFFT { get; set; }
+            public bool SupportsDDJB { get; set; }
+            public bool SupportsLWTT { get; set; }
+            public Flight Flight { get; set; }
 
-        public BoardingGate(string gateName, bool supportsCFFT, bool supportsDDJB, bool supportsLWTT)
-        {
-            GateName = gateName;
-            SupportsCFFT = supportsCFFT;
-            SupportsDDJB = supportsDDJB;
-            SupportsLWTT = supportsLWTT;
+            public BoardingGate(string gateName, bool supportsCFFT, bool supportsDDJB, bool supportsLWTT)
+            {
+                GateName = gateName;
+                SupportsCFFT = supportsCFFT;
+                SupportsDDJB = supportsDDJB;
+                SupportsLWTT = supportsLWTT;
+            }
+
+            public double CalculateFees()
+            {
+                return Flight?.CalculateFees() ?? 0;
+            }
+
+            public override string ToString()
+            {
+                return "Gate: " + GateName +
+                    "\tSupports CFFT: " + SupportsCFFT +
+                    "\tSupports DDJB: " + SupportsDDJB +
+                    "\tSupports LWTT: " + SupportsLWTT;
+            }
+
         }
-
-        public double CalculateFees()
-        {
-            return Flight?.CalculateFees() ?? 0;
-        }
-
-        public override string ToString()
-        {
-            return "Gate: " + GateName +
-                "\tSupports CFFT: " + SupportsCFFT +
-                "\tSupports DDJB: " + SupportsDDJB +
-                "\tSupports LWTT: " + SupportsLWTT;
-        }
-
     }
-}
+
+
+
+
